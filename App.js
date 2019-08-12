73818967cba2334806f6b561ffab2f38617d0c11
Try <khovct@gmail.com>
2019-08-11 21:38:48 -0700

    installed native-base and added styling for the home screen

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import HomeScreen from './screens/HomeScreen';
+import AddNewContactScreen from './screens/AddNewContactScreen';
+import EditContactScreen from './screens/EditContactScreen';
+import ViewContactScreen from './screens/ViewContactScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const MainNavigator = createStackNavigator(
+  {
+    Home: { screen: HomeScreen },
+    Add: { screen: AddNewContactScreen },
+    Edit: { screen: EditContactScreen },
+    View: { screen: ViewContactScreen }
   },
-});
+  {
+    defaultNavigation: {
+      headerTintColor: "#FFF",
+      headerStyle: {
+        backgroundColor: "#B83227"
+      },
+      headerTitleStyle: {
+        color: "#FFF"
+      }
+    }
+  }
+);
+
+const App = createAppContainer(MainNavigator);
+export default App;
---
+++ screens/HomeScreen.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, AsyncStorage } from 'react-native';
+import { Card } from 'native-base';
+import { Entypo } from '@expo/vector-icons';
+
+export default class HomeScreen extends React.Component {
+    static navigationOption = {
+        title: "Contact App"
+    }
+    
+    render() {
+        return (
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.floatButton} onPress={() => this.props.navigation.navigate("Add")}>
+                <Entypo name="plus" size={30} color="#fff"/>
+            </TouchableOpacity>
+        </View>
+        )
+    } 
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff"
+    },
+    listItem: {
+      flexDirection: "row",
+      padding: 20
+    },
+    iconContainer: {
+      width: 50,
+      height: 50,
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: "#B83227",
+      borderRadius: 100
+    },
+    contactIcon: {
+      fontSize: 28,
+      color: "#fff"
+    },
+    infoContainer: {
+      flexDirection: "column"
+    },
+    infoText: {
+      fontSize: 16,
+      fontWeight: "400",
+      paddingLeft: 10,
+      paddingTop: 2
+    },
+    floatButton: {
+      borderWidth: 1,
+      borderColor: "rgba(0,0,0,0.2)",
+      alignItems: "center",
+      justifyContent: "center",
+      width: 60,
+      position: "absolute",
+      bottom: 10,
+      right: 10,
+      height: 60,
+      backgroundColor: "#B83227",
+      borderRadius: 100
+    }
+  });
