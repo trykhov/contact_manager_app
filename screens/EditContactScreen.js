bbe6e96323bc5afccaacd02d3f3c3dbd23804dc2
Try <khovct@gmail.com>
2019-08-15 16:35:22 -0700

    editing the EditContactScreen

--- screens/EditContactScreen.js
+++ screens/EditContactScreen.js
@@ -1,7 +1,67 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableWithoutFeedback, AsyncStorage, Alert } from 'react-native';
+import { Form, Item, Input, Label, Button } from 'native-base';
 
 export default class EditNewContactScreen extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      lastName: "",
+      phoneNumber: "",
+      address: "",
+      email: "",
+      key: ""
+    }
+  }
+
+  componentDidMount() {
+    const { navigation } = this.props;
+    navigation.addListener("willFocus", () => {
+      let key = this.props.navigation.getParam("key", "");
+      this.getContact();
+    })
+  };
+  
+  getContact = async key => {
+    await AsyncStorage.getItem(key)
+      .then(contactJSONString => {
+        let contact = JSON.parse(contactJSONString);
+        contact["key"] = key; // set key in this object
+        this.setState(contact);
+      })
+      .catch(err => console.log(err))
+  };
+
+  updateContact = async key => {
+    if(
+      this.state.firstName !== "" && 
+      this.state.lastName !== "" &&
+      this.state.phoneNumber !== "" &&
+      this.state.address !== "" &&
+      this.state.email !== "" &&
+      this.state.key !== ""
+      ) {
+        let contact = {
+          firstName: this.state.firstName,
+          lastName: this.state.lastName,
+          phoneNumber: this.state.phoneNumber,
+          address: this.state.address,
+          email: this.state.email,
+          key: this.state.key
+        }
+        await AsyncStorage.mergeItem(key, JSON.stringify(contact))
+          .then(() => {
+            this.props.navigation.goBack()
+          })
+          .catch(err => console.log(err))
+      }
+  }
+
+  static navigationOptions = {
+    title: "Edit Contact"
+  }
 
   render() {
     return (
@@ -15,8 +75,18 @@ export default class EditNewContactScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    margin: 10
   },
+  inputItem: {
+    margin: 10
+  },
+  button: {
+    backgroundColor: "#B83227",
+    marginTop: 40
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold"
+  }
 });
